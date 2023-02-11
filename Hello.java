888977fa9e74e62644a4713abcd713961ff75a37
kishorebullten <48083344+kishorebullten@users.noreply.github.com>
2023-02-11 22:51:44 +0530

    Update and rename helloo.js to Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello{
+public static void main(String[] args){
+  while(true){
+    try{
+      System.out.println("sleeping");
+      Thread.sleep(1000);
+    } catch(Exception ex){
+    
+    }
+  }
+}
+}
